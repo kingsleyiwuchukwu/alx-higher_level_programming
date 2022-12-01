96658fde4c96b71e3671fd507b521be7d29f2ebf
kingsleyiwuchukwu <kingsleyiwuchukwu0@gmail.com>
2022-12-01 04:01:59 +0100

    Task 9-13

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
-def print_last_digit(number):
-    last_digit = (number % 10) if number >= 0 else ((number * -1) % 10)
-    print(last_digit, end='')
-    return (last_digit)
+def uppercase(str):
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
+    print()
